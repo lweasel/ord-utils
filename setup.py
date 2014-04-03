d739a88091354d8f56534f7a503cbd6cfd0c068e
Owen Dando <owen.dando@ed.ac.uk>
2014-04-03 16:56:43 +0100

    Require 'schema' package.

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from distutils.core import setup
 setup(
     name="OrdUtils",
     version="0.1",
-    packages=["ordutils"]
+    packages=["ordutils"],
+    install_requires=[
+        "schema >= 0.2.1"
+    ],
 )
