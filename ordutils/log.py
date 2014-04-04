8b35a20556421a361265794c1c5d772d8ffb3048
Owen Dando <owen.dando@ed.ac.uk>
2014-04-04 12:53:15 +0100

    Add docstrings, and chnage method name getLogger -> get_logger.

--- ordutils/log.py
+++ ordutils/log.py
@@ -1,3 +1,9 @@
+"""
+Utility functions for logging messages. Exports:
+
+get_logger: Return a logger with a specified severity threshold.
+"""
+
 import logging
 
 LEVELS = {
@@ -9,7 +15,17 @@ LEVELS = {
     }
 
 
-def getLogger(stream, level):
+def get_logger(stream, level):
+    """
+    Return a Logger instance with the specified severity threshold.
+
+    Return a Logger instance with the specified severity threshold, where the
+    threshold level should be a key of the 'LEVELS' dictionary. Log messages
+    will contain the current time and message severity level.
+    stream: Output stream to which the logger will write messages.
+    level: Severity threshold level, which should be a key of the 'LEVELS'
+    dictionary.
+    """
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
     handler = logging.StreamHandler(stream)
